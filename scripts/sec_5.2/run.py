39dff981e2bda59aa8dc760a7cdadf0fda609830
Benjamin W. <benjaminsw@users.noreply.github.com>
2025-06-24 16:17:19 +0100

    added pvi

--- scripts/sec_5.2/run.py
+++ scripts/sec_5.2/run.py
@@ -26,7 +26,7 @@ PROBLEMS = {
     'xshape': XShape,
 }
 
-ALGORITHMS = ['nf_pvi','wgf_gmm'] #,'pvi'] #, 'sm', 'svi', 'uvi']
+ALGORITHMS = ['nf_pvi','wgf_gmm','pvi'] #, 'sm', 'svi', 'uvi']
 
 def visualize(key, 
               ids,
